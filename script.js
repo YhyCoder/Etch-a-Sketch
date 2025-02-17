f56e1ead962599a352e5da66962d8cb4d5926cfa
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-02-17 16:15:59 +0330

    Add hover feature to change color of squares

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+// Create 16 * 16 grid
 function createGrid() {
-  const container = document.querySelector(".container");
   const squareSize = `${100 / 16}%`;
   for(let i = 0; i < 16 * 16; i++) {
     const square = document.createElement("div");
     square.style.width = squareSize;
     square.style.height = squareSize;
     square.style.border = "2px solid #fff";
-    container.appendChild(square);
+    document.querySelector(".container").appendChild(square);
   }
 }
 
-createGrid();
+// Change squares color when hover them
+function changeSquareColor() {
+  createGrid();
+  const squares = document.querySelectorAll(".container > div");
+  squares.forEach(square => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "#393E46";
+    });
+  });
+}
+
+changeSquareColor();
